ab7c901007889bb881d77005007805340bccd971
David <david.roquain@edu.esiee-it.fr>
2022-04-06 13:43:12 +0200

    Début java

---
+++ server/NFsTeam/src/test/java/com/example/NFsTeam/NFsTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NFsTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NFsTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
