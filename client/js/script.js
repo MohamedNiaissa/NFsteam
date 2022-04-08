588e0c85b5e6969e992240e75ce17a70f0c3c9b1
Mohamed <mohameddrame907@gmail.com>
2022-04-08 10:53:37 +0200

    petit changement

--- client/js/script.js
+++ client/js/script.js
@@ -1,9 +1,12 @@
 function callAPI(){
+    let liste = [];
     let httpRequest = new XMLHttpRequest();
     httpRequest.open('GET','http://localhost:8080/users');
     httpRequest.send();
     httpRequest.onload= function(){
         alert(httpRequest.responseText);
+        liste.push(httpRequest.responseText)
+        alert(liste[0])
     }
     console.log("inside the .js file")
 }
