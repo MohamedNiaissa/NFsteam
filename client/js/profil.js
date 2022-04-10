47d7a0ac6a7323663b0eff3a33030a2f498066cc
Mohamed <mohameddrame907@gmail.com>
2022-04-10 15:33:35 +0200

    modification nom user possible

--- client/js/profil.js
+++ client/js/profil.js
@@ -2,9 +2,13 @@ let supp = document.querySelector('.suppr');
 let profilTitre = document.querySelector(".profilTitre");
 let nomProfil = localStorage.getItem("nom");
 profilTitre.innerHTML += " " + nomProfil;
+let btn_nommodif = document.querySelector('.btn_nommodif')
+let newnom = document.querySelector('#idnom');
+
+
+let headers = {}
 
 supp.addEventListener("click",function(){
-    var headers = {}
     let url = `http://localhost:8080/users/removeuser/${nomProfil}`
     fetch(url, {
         method : "GET",
@@ -13,4 +17,16 @@ supp.addEventListener("click",function(){
     })
     
     localStorage.clear()
+})
+
+btn_nommodif.addEventListener('click',function(){
+    console.log(nomProfil);
+    console.log(newnom)
+    localStorage.setItem("nom",newnom.value)
+    let url = `http://localhost:8080/users/updateusernom/${nomProfil}/${newnom.value}`;
+    fetch(url, {
+        method : "GET",
+        mode: 'cors',
+        headers: headers
+    })
 })
