57da0fc33c2ccf576937bbf2ca16b2cfea17e886
Mohamed <mohameddrame907@gmail.com>
2022-04-13 10:03:50 +0200

    jeux apparait en fonction du filtre prix

--- client/js/Slider.js
+++ client/js/Slider.js
@@ -1,8 +1,9 @@
 let slide = document.querySelector(".slider");
 let prixFiltre = document.querySelector(".prixFiltre")
 
-
 slide.addEventListener('change',function(){
+    let divCopie = document.querySelectorAll(".Copie")
+
     let categorie = document.querySelector(".categorie");
 
     prixFiltre.innerHTML = slide.value+" $";
@@ -14,20 +15,48 @@ slide.addEventListener('change',function(){
         httpRequest.send();
         httpRequest.onload= function(){
             liste.push(JSON.parse(httpRequest.responseText))
-    
+
+            for(let i = 0;i < divCopie.length; i++){
+                divCopie[i].style.display = "none";
+            }
+
             for(let i = 0;i < liste[0].length; i++){
-                console.log(liste[0][i].prixArt)
-
-
-
+                let currentElem = liste[0][i];
+                console.log(liste[0][i])
                 if(liste[0][i].prixArt <= slide.value){
+                    let divelem = document.createElement("div");
+                    divelem.className = "Copie";
+                    divelem.className += " div";
+                    divelem.id = "id"+liste[0][i].nomArt;
+                    categorie.appendChild(divelem)
 
-                }else{
-                    let id = "id"+liste[0][i].nomArt;
-                    console.log(id)
-                    document.querySelector(id).remove;
-                    liste[0].remove(i);
+                    let lienelemTag = document.createElement("a");
+                    lienelemTag.className = "clickable";
+                    lienelemTag.innerHTML = currentElem.tag;
 
+                    let imgelem = document.createElement('img');
+                    imgelem.setAttribute('src',currentElem.imgArt);
+                    imgelem.className = 'imgProduct';
+
+
+                    let lienelem = document.createElement("a");
+                    lienelem.className = "clickable";
+                    lienelem.innerHTML = currentElem.tag;
+                    lienelem.setAttribute("href","produit.html")
+                    lienelem.id = currentElem.nomArt;
+                    lienelem.innerHTML = currentElem.nomArt;
+
+                    let paraPrix = document.createElement('p');
+                    paraPrix.innerHTML = currentElem.prixArt + "$";
+
+                    divelem.appendChild(lienelemTag);
+                    divelem.appendChild(imgelem);
+                    divelem.appendChild(lienelem);
+                    divelem.appendChild(paraPrix);
+                }
+                let elemdiv = document.querySelectorAll('div');
+                for(let i= 0; i<elemdiv.length;i++){
+                    elemdiv[i].remove;
                 }
             }
         }
